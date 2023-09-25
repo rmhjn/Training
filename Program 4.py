04f520973be041222989432a1d621d314ce422a7
rmhjn <145959361+rmhjn@users.noreply.github.com>
2023-09-25 16:39:44 +0530

    New

---
+++ Program 4.py
@@ -0,0 +1,26 @@
+class Queue:
+    def __init__(self):
+        self.items = []
+    def enqueue(self, item):
+        self.items.append(item)
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise IndexError("Cannot dequeue from an empty queue.")
+    def is_empty(self):
+        return len(self.items) == 0
+# Example usage
+queue = Queue()
+queue.enqueue(10)
+queue.enqueue(20)
+queue.enqueue(30)
+queue.enqueue(40)
+queue.enqueue(50)
+
+print("Current Queue:", queue.items)
+dequeued_item = queue.dequeue()
+print("Dequeued item:", dequeued_item)
+dequeued_item = queue.dequeue()
+print("Dequeued item:", dequeued_item)
+print("Updated Queue:", queue.items)
